6b454bde3920172a14ae307426aa1d59c7dd4813
Guillem Lefait <guillem@datamq.com>
2025-08-29 14:13:55 -0400

    async fetcher

--- src/tlds_monitoring/root/details.py
+++ src/tlds_monitoring/root/details.py
@@ -2,6 +2,9 @@ import httpx
 import json
 import os
 import click
+import aiohttp
+import asyncio
+
 
 from tlds_monitoring.root import ROOT_TLD_FILE, DOMAIN_DIR_PATH, ROOT_TLD_DETAILS_FILE
 
@@ -11,10 +14,37 @@ def get_tld_details_through_rdap_files(root_tlds):
     root_tlds_details = {}
     with httpx.Client() as client:
         for tld in root_tlds:
+            print(tld)
             req = client.get(f"https://rdap.iana.org/domain/{tld}")
             root_tlds_details[tld] = req.json()
-    # TODO: slow. 02m 30s => multithread ?
+    # TODO: slow. 02m 30s => async ?
     return root_tlds_details
+
+
+async def fetch(session: aiohttp.ClientSession, sem, url, tld):
+    async with sem, session.get(url) as response:
+        return tld, await response.json()
+
+
+async def async_get_tld_details_through_rdap_files(root_tlds):
+    # for each tld, get details
+    timeout_seconds = 30  # let's be super patient
+    sem = asyncio.Semaphore(10)  # let's be super nice (aiohttp default is 100)
+    custom_timeout = aiohttp.ClientTimeout(
+        total=0,
+        sock_connect=timeout_seconds,
+        sock_read=timeout_seconds,
+        connect=timeout_seconds,
+    )
+    tasks = []
+    async with aiohttp.ClientSession(timeout=custom_timeout) as session:
+        for tld in root_tlds:
+            tasks.append(
+                fetch(session, sem, f"https://rdap.iana.org/domain/{tld}", tld)
+            )
+        fetched = await asyncio.gather(*tasks)
+
+    return {k: v for k, v in fetched}
 
 
 @click.command()
@@ -25,14 +55,20 @@ def main(data_path: str):
     # load existing root tlds
     with open(os.path.join(data_path, f"{ROOT_TLD_FILE}.json"), "r") as r:
         root_tlds = json.load(r)
-        tld_details = get_tld_details_through_rdap_files(root_tlds)
+        loop = asyncio.get_event_loop()
+        tld_details = loop.run_until_complete(
+            async_get_tld_details_through_rdap_files(root_tlds)
+        )
+        # tld_details = get_tld_details_through_rdap_files(root_tlds)
+
         # write the individual details
         domain_path = os.path.join(data_path, DOMAIN_DIR_PATH)
         os.makedirs(domain_path, exist_ok=True)
         for tld, tld_detail in tld_details.items():
             with open(os.path.join(domain_path, f"{tld}.json"), "w") as f:
                 json.dump(tld_detail, indent=2, fp=f)
-        # write the aggregated result
+
+        # # write the aggregated result
         with open(os.path.join(data_path, f"{ROOT_TLD_DETAILS_FILE}.json"), "w") as f:
             json.dump(tld_details, indent=2, fp=f)
 
